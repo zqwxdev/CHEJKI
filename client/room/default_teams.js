25927d4c5ed346773ed68c619e1c4a23c1e1dcae
zqwxdev <narezchiksprintera@gmail.com>
2024-12-25 18:12:45 +0300

    Update default_teams.js

--- client/room/default_teams.js
+++ client/room/default_teams.js
@@ -5,8 +5,8 @@ import { Teams } from 'pixel_combats/room';
 
 export const RED_TEAM_NAME = "Red";
 export const BLUE_TEAM_NAME = "Blue";
-export const RED_TEAM_DISPLAY_NAME = "Teams/Red";
-export const BLUE_TEAM_DISPLAY_NAME = "Teams/Blue";
+export const RED_TEAM_DISPLAY_NAME = "СНЕЖКИ";
+export const BLUE_TEAM_DISPLAY_NAME = "ТГ КАНАЛ newsofpc2";
 export const BLUE_TEAM_SPAWN_POINTS_GROUP = 1;
 export const RED_TEAM_SPAWN_POINTS_GROUP = 2;
 export const BLUE_TEAM_COLOR = new Color(0, 0, 1, 0);
